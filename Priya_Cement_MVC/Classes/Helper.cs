892c9f699ac01d1c697c4b3732a5b14c891c1402
TICsoftware <tic.access@ticworks.com>
2026-08-04 13:23:46 +0530

    changed done

--- Priya_Cement_MVC/Classes/Helper.cs
+++ Priya_Cement_MVC/Classes/Helper.cs
@@ -185,7 +185,7 @@ namespace Priya_Cement_MVC.Classes
                     "password"
                 );
 
-                message.From = new MailAddress("ticworks2022@gmail.com", "Nekta");
+                message.From = new MailAddress("ticworks2022@gmail.com", Priya Cement);
                 message.To.Add("ashabbir72@gmail.com");
 
                 message.Subject = strSubject;
