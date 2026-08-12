d640a50c47311f060a83b6538f9e08f2a5b8c9fd
unknown <shabbir.ansari@ticworks.com>
2026-08-12 17:52:47 +0530

    change

--- Priya_Cement_MVC/Program.cs
+++ Priya_Cement_MVC/Program.cs
@@ -56,20 +56,20 @@ builder.Services.AddAuthentication("MyCookieAuth")
 builder.Services.AddAuthorization();
 
 //uncomment while live start
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(20); // session timeout
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
+// builder.Services.AddSession(options =>
+// {
+//     options.IdleTimeout = TimeSpan.FromMinutes(20); // session timeout
+//     options.Cookie.HttpOnly = true;
+//     options.Cookie.IsEssential = true;
+// });
 
- builder.Services.AddAntiforgery(options =>
-{
-    options.Cookie.Name = "SecureToken";
-    options.Cookie.HttpOnly = true;
-   options.Cookie.SecurePolicy =
-        CookieSecurePolicy.Always;  
-});
+//  builder.Services.AddAntiforgery(options =>
+// {
+//     options.Cookie.Name = "SecureToken";
+//     options.Cookie.HttpOnly = true;
+//    options.Cookie.SecurePolicy =
+//         CookieSecurePolicy.Always;  
+// });
 //uncomment while live end
 
 builder.Services.AddHttpClient();
