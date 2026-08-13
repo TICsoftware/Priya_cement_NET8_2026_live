cfdf30c7b63e95b67df92aad9cacf958ff7e57fa
TICsoftware <tic.access@ticworks.com>
2026-08-13 16:27:11 +0530

    changes dopne

---
+++ Priya_Cement_BusinessLogic/Entity/SolutionsEnquiry.cs
@@ -0,0 +1,22 @@
+namespace Priya_Cement_BusinessLogic.Entity
+{
+    public class SolutionsEnquiry
+    {
+        public int SolutionsId { get; set; }
+        public string FullName { get; set; }
+        public string MobileNumber { get; set; }
+        public string WhatsappNumber { get; set; }
+        public string EmailId { get; set; }
+        public string Gender { get; set; }
+        public string AgeGroup { get; set; }
+        public string District { get; set; }
+        public string TownVillage { get; set; }
+        public string CurrentOccupation { get; set; }
+        public string CurrentOccupationOthers { get; set; }
+        public string OwnShopCommercialSpace { get; set; }
+        public string PreviouslyRunBusiness { get; set; }
+        public string SpaceForStoreSetup { get; set; }
+        public string StoreSizeSqft { get; set; }
+        public string PreferredTimeForContact { get; set; }
+    }
+}
